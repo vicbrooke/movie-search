4aaac15f56f555f9b65dc82d7242758c39119c25
MBailey114 <baileymichael114@gmail.com>
2022-10-26 15:25:06 +0100

    Added Search functionality

---
+++ script.js
@@ -0,0 +1,46 @@
+const movieBox = document.querySelector("#movieName");
+const countryBox = document.querySelector("#country");
+const movieDropDown = document.querySelector("#movieDropdown");
+
+let cards = Array.from(movieDropDown.childNodes);
+
+
+
+let movieArray = []
+if(movieArray.length === 0){
+    movieDropDown.style.display = "none";
+}
+
+textBox.addEventListener("input", () => {
+    if(textBox.value === ""){
+        movieArray = [];
+
+    } else {
+        fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${textBox.value}&country=${countryBox.value}`)
+        .then(response => response.json())
+        .then(data =>  movieArray = data.results)
+        .catch(error => console.log(error));
+
+        while(movieArray.length > 5){
+            movieArray.pop();
+        }
+
+        let count = 0;
+
+        for(let i of movies){
+            cards[count].firstChild.src = i.picture;
+            cards[count].lastChild.innerText = i.name;
+            count++;
+        }
+
+        for(let i = count; i < 4; i++){
+            cards[i].style.display = "none";
+        }
+
+        movieDropDown.style.display = "";
+
+
+
+    }
+})
+
