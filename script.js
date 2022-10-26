45a750d579ffd88c906706d6e5a4d727e193fcb2
MBailey114 <baileymichael114@gmail.com>
2022-10-26 17:05:54 +0100

    Optimised Searching so it doesnt search on every keystroke

--- script.js
+++ script.js
@@ -23,47 +23,16 @@ if(movieArray.length === 0){
     movieDropDown.style.display = "none";
 }
 
+let inputDisabled = false;
+let registeredInput = false
+
 movieBox.addEventListener("input", () => {
-    if(movieBox.value === ""){
-        movieArray = [];
-        for(let i of cards){
-            i.firstChild.src = "";
-            i.lastChild.innerText = "";
-        }
-
-    } else {
-
-        gather().then(result => movieArray = result).catch(error => console.log(error));
-
-        console.log(movieArray);
-
-        while(movieArray.length > 5){
-            movieArray.pop();
-        }
-
-        let count = 0;
-
-
-        for(let i of movieArray){
-            try{
-                cards[count].firstChild.src = i.picture;
-            } catch{
-                cards[count].firstChild.src = "https://w7.pngwing.com/pngs/807/819/png-transparent-http-404-computer-icons-world-wide-web-text-trademark-computer-thumbnail.png";
-            }
-            
-            cards[count].style.display = "";
-            cards[count].lastChild.innerText = i.name;
-            count++;
-
-        }
-
-        for(let i = count; i < 4; i++){
-            console.log(cards[i]);
-            cards[i].style.display = "none";
-        }
-
-        movieDropDown.style.display = "";
-
+    if(!inputDisabled){
+        inputDisabled = true;
+        setTimeout(function(){inputDisabled = false;
+        search()}, 1000);
+        search()
+       
 
 
     }
@@ -96,6 +65,51 @@ async function gather(){
     const response = await fetch(`https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movieBox.value}&country=${countryBox.value}`,options);
     const data = await response.json();
     return data.results;
+
+}
+
+function search(){
+    if(movieBox.value === ""){
+        movieArray = [];
+        for(let i of cards){
+            i.firstChild.src = "";
+            i.lastChild.innerText = "";
+        }
+
+    } else {
+
+        gather().then(result => movieArray = result).catch(error => console.log(error));
+
+        console.log(movieArray);
+
+        while(movieArray.length > 5){
+            movieArray.pop();
+        }
+
+        let count = 0;
+
+
+        for(let i of movieArray){
+            try{
+                cards[count].firstChild.src = i.picture;
+            } catch{
+                cards[count].firstChild.src = "https://w7.pngwing.com/pngs/807/819/png-transparent-http-404-computer-icons-world-wide-web-text-trademark-computer-thumbnail.png";
+            }
+            
+            cards[count].style.display = "";
+            cards[count].lastChild.innerText = i.name;
+            count++;
+
+        }
+        console.log(count);
+
+        for(let i = count; i <= 4; i++){
+            console.log(cards[i]);
+            cards[i].style.display = "none";
+        }
+
+        movieDropDown.style.display = "";
+    }
 
 }
 
